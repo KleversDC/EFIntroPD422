40d812bcde65c5e7a4168af4473d4ff1d389e11d
KleversDC <klevers170@gmail.com>
2025-03-14 19:44:10 +0200

    add data ersess labreri

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EFIntroPD422
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFIntroPD422
 {
     internal class Program
     {
@@ -10,12 +12,38 @@
 
             context.SaveChanges();
          
+            var flights = context.Flights.Include(f=>f.Airplane).
+                Where(f=>f.ArrivalCity == "Kyiv").OrderBy(f=> f.DepartureTime).ToList();
+            foreach (var f in flights)
+            {
+                Console.WriteLine($"Flight: #{f.Id}, from {f.DepartureCity} to {f.ArrivalCity} at" +
+                    $"{f.ArrivalTime.ToShortDateString()} airplein {f.Airplane?.Model}");
+            }
+
+
 
             Console.WriteLine("All of clients : ");
             foreach (var client in context.Clients)
             {
                 Console.WriteLine($"Client : {client.Name} - {client.Email} - {client.Birthdate}");
             }
+
+            var cclient = context.Clients.Find(1);
+            context.Entry(cclient).Collection(c => c.Flights).Load();
+            Console.WriteLine($"Client : {cclient.Name} has {cclient.Flights?.Count}");
+
+            var Flightss = context.Flights
+                .Include(f => f.Clients)
+                .OrderBy(f => f.DepartureTime).ToList();
+
+            foreach (var f in Flightss)
+            {
+                Console.WriteLine($"#{f.Id}, From {f.ArrivalCity} to {f.ArrivalTime.ToShortDateString()}" +
+                    $"Airplane: {f.Airplane?.Model} - with {f.Clients?.Count}");
+
+
+            }
+
         }
     }
 }
